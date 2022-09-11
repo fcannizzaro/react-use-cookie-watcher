9281ec0a5b6fbcbafd4589f0ea32d2b59bec32b3
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2022-09-11 13:49:42 +0200

    fix regex to support multiple cookies string

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -2,11 +2,13 @@ import {beforeEach, describe, expect, test} from 'vitest';
 import {render, screen} from '@testing-library/react';
 import {useCookieWatcher} from "./index";
 
+const COOKIE_NAME = "cookie-token"
+
 const Demo = () => {
-    const value = useCookieWatcher("token", {
+    const value = useCookieWatcher(COOKIE_NAME, {
         valueOnly: true
     });
-    const exist = useCookieWatcher("token");
+    const exist = useCookieWatcher(COOKIE_NAME);
     return <div>
         <p>{exist ? "cookie-ok" : "cookie-missing"}</p>
         <p>{value}</p>
@@ -16,11 +18,11 @@ const Demo = () => {
 describe("useCookieWatcher test", () => {
 
     beforeEach(() => {
-        document.cookie = "token=;Max-Age=0";
+        document.cookie = `${COOKIE_NAME}=;Max-Age=0`;
     });
 
     test("check if initial cookie is detected", () => {
-        document.cookie = "token=value";
+        document.cookie = `${COOKIE_NAME}=value`;
         render(<Demo/>);
         expect(screen.getByText("cookie-ok")).toBeDefined()
         expect(screen.getByText("value")).toBeDefined()
@@ -35,9 +37,9 @@ describe("useCookieWatcher test", () => {
     test("check if detection live is working", () => {
         render(<Demo/>);
         expect(screen.getByText("cookie-missing")).toBeDefined()
-        setTimeout(() => document.cookie = "token=value", 1000);
+        setTimeout(() => document.cookie = `${COOKIE_NAME}=value`, 1000);
         setTimeout(() => {
-            document.cookie = "token=value";
+            document.cookie = `${COOKIE_NAME}value`;
             expect(screen.getByText("cookie-ok")).toBeDefined()
             expect(screen.getByText("value")).toBeDefined()
         }, 300);
